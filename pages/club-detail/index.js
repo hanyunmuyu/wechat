987b15e3f3a5aab6ef035bf89583160f4d858ea8
hanyun <1355081829@qq.com>
2018-06-02 21:11:34 +0800

    修改社团活动详情页面

--- pages/club-detail/index.js
+++ pages/club-detail/index.js
@@ -10,6 +10,57 @@ Page({
                 id: 1,
                 title: "成员"
             }
+        ],
+        activityDataList:[
+            {
+                id:1,
+                title:"哈哈哈，二货程序员",
+                userName:"寒云",
+                userId:1,
+                description:"简单的描述文字。。。。。你们这些逗比简单的描述文字。。。。。你们这些逗比简单的描述文字。。。。。你们这些逗比简单的描述文字。。。。。你们这些逗比简单的描述文字。。。。。你们这些逗比"
+            },
+            {
+                id:1,
+                title:"哈哈哈，二货程序员",
+                userName:"寒云",
+                userId:1,
+                description:"简单的描述文字。。。。。你们这些逗比"
+            },
+            {
+                id:1,
+                title:"哈哈哈，二货程序员",
+                userName:"寒云",
+                userId:1,
+                description:"简单的描述文字。。。。。你们这些逗比"
+            },
+            {
+                id:1,
+                title:"哈哈哈，二货程序员",
+                userName:"寒云",
+                userId:1,
+                description:"简单的描述文字。。。。。你们这些逗比"
+            },
+            {
+                id:1,
+                title:"哈哈哈，二货程序员",
+                userName:"寒云",
+                userId:1,
+                description:"简单的描述文字。。。。。你们这些逗比"
+            },
+            {
+                id:1,
+                title:"哈哈哈，二货程序员",
+                userName:"寒云",
+                userId:1,
+                description:"简单的描述文字。。。。。你们这些逗比"
+            },
+            {
+                id:1,
+                title:"哈哈哈，二货程序员",
+                userName:"寒云",
+                userId:1,
+                description:"简单的描述文字。。。。。你们这些逗比"
+            }
         ]
     },
     select: function (e) {
